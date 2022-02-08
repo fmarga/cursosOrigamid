4c52a457fe15fe4bb1c30703b3f11b019242a90f
fmarga <marga.fmartins@gmail.com>
2022-02-08 15:54:46 -0300

    made a fake api and implemented it to the code

--- cursoJavascript/js/main.js
+++ cursoJavascript/js/main.js
@@ -2,8 +2,10 @@ import initScrollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
+import initFetchAnimais from "./modules/fetch-animais.js";
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
+initFetchAnimais();
