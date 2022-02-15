951f74fadbd73623ade86bee71c8e4cd8cee6565
fmarga <marga.fmartins@gmail.com>
2022-02-15 19:08:01 -0300

    add some interactive tools

--- cursoJavascript/js/main.js
+++ cursoJavascript/js/main.js
@@ -3,9 +3,13 @@ import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
 initFetchAnimais();
+initModal();
+initTooltip();
